202f74d5860dda5490adeb8cdf4c6dedb6c55af5
t-kusakabe <ttmac0704@gmail.com>
2022-03-09 01:06:41 +0900

    seedの追加

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,11 +1,13 @@
 import { prisma } from '../lib/prisma';
 
 const main = async () => {
-  data: [
-    { title: 'sample task 1', done: true },
-    { title: 'sample task 2', done: true },
-    { title: 'sample task 3', done: false }
-  ];
+  await prisma.task.createMany({
+    data: [
+      { title: 'sample task 1', done: true },
+      { title: 'sample task 2', done: true },
+      { title: 'sample task 3', done: false }
+    ]
+  });
 };
 
 main().catch((e) => {
