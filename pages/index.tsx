ebeea5c744120b8d4e169b9faa606eda45dc8c6e
t-kusakabe <ttmac0704@gmail.com>
2022-03-09 17:33:35 +0900

    ひとまず一覧作るところまで

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,23 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-// import styles from '../styles/Home.module.css';
+import { gql, useQuery } from '@apollo/client';
+
+const AllTasksQuery = gql`
+  query {
+    tasks {
+      id
+      title
+      done
+    }
+  }
+`;
 
 const Home: NextPage = () => {
+  const { data, loading, error } = useQuery(AllTasksQuery);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
   return (
     <div>
       <Head>
@@ -12,7 +27,17 @@ const Home: NextPage = () => {
       </Head>
 
       <main>
-        <h1>Hello World!</h1>
+        <h1>ToDo</h1>
+
+        <ul>
+          {data.tasks.map((task: any) => {
+            return (
+              <li key={task.id}>
+                <p>{task.title}</p>
+              </li>
+            );
+          })}
+        </ul>
       </main>
     </div>
   )
